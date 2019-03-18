08a186acacf91db2d004c2f8ae2101b21ecba3a9
Tino Andrijic <tino@andrijic.com>
2019-03-18 22:30:30 +0100

    ispravak

--- Lab2/tcp_client.py
+++ Lab2/tcp_client.py
@@ -3,8 +3,8 @@ import socket
 
 client_socket = socket.socket()
 host = socket.gethostname()
-port = 9999
+port = 9999 
 
-client_socket.connect(('localhost', port))
+client_socket.connect(('localhost',port))
 print client_socket.recv(1024)
 client_socket.close()
