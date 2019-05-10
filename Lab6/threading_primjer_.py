6ed228947cac873a5be3f2dc2ffecfb31564b9eb
Tino Andrijic <tino@andrijic.com>
2019-05-10 22:46:21 +0200

    dio lab6

---
+++ Lab6/threading_primjer_.py
@@ -0,0 +1,25 @@
+#threading_primjer_.py
+#Mrezno programiranje LABno6 2019
+
+import thread
+import time
+# Definicija funkcije za nit
+
+def print_time( threadName, delay): 
+    count = 0
+    while count < 5:
+        time.sleep(delay)
+        count += 1
+        print "%s: %s" % ( threadName, time.ctime(time.time()) )
+
+# Kreiramo dvije niti
+
+try:
+    thread.start_new_thread( print_time, ("Thread-1", 2, ) )
+    thread.start_new_thread( print_time, ("Thread-2", 4, ) )
+except:
+    print "Greska: ne mogu pokrenuti nit!!"
+
+# Cekaj dok se sve niti ne izvrse
+while 1:
+    pass
