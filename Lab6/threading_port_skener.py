7bbd16060cc20720725a0933b3e0e521af1ce197
Tino Andrijic <tino@andrijic.com>
2019-06-13 17:31:46 +0200

    Lab6

---
+++ Lab6/threading_port_skener.py
@@ -0,0 +1,56 @@
+import socket
+import datetime
+
+from local_machine_info import print_machine_info
+from Queue import Queue
+
+import threading
+import os
+
+
+def check_ping(hostname):
+    response = os.system("ping -c1 " + hostname)	
+    return response
+
+def scanPort(port):
+	print("Skeniram port %d") % port
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	result = s.connect_ex((host,port))
+	if result == 0:
+		print "Port je otvoren"
+	else:
+		print "Port je zatvoren"
+	s.close()
+	
+
+def worker():
+    while True:
+        port = q.get()
+        scanPort(port)
+        q.task_done()
+
+
+datum = datetime.datetime.now()
+print(datum)
+print_machine_info()
+
+host = raw_input("Upisite adresu hosta koju cemo testirati: ")
+print("Od kojeg do kojeg porta zelite napraviti skeniranje")
+start = raw_input("Pocetni port: ")
+end = raw_input("Zavrsni port: ")
+print("Provjeravam da li je host dostupan...")
+
+q = Queue()
+if check_ping(host) == 0:
+	for i in range(3):
+		t = threading.Thread(target=worker)
+		t.daemon = True
+		t.start()
+
+	for port in range(int(start), int(end)+1):
+		q.put(port)
+else:
+	print("Host nije dostupan")
+	
+q.join()       # block until all tasks are done
