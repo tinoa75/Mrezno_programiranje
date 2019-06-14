a08cbb2c44ffa2864e933a2984b112467616fc76
Tino Andrijic <tino@andrijic.com>
2019-06-14 21:27:24 +0200

    Lab8

---
+++ Lab8/HTTP.py
@@ -0,0 +1,11 @@
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "HTTP server na portu: ", PORT
+httpd.serve_forever()
